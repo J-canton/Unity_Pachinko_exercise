85141e842e4e88bde5cee79066900fae8b98699f
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2020-12-14 16:05:32 +0100

    builds

---
+++ Assets/Scripts/HoleBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoleBehaviour : MonoBehaviour
+{
+    public int award = 0;
+    public int total = 0;
+    public Text CounterText;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        HoleManager.TotalValue += award * 1;
+        CounterText.text = "TOTAL PRIZE: " + HoleManager.TotalValue;
+        Destroy(other);
+        Debug.Log("BOLA EN EL AGUJERO " + total);
+    }
+
+}
---
+++ Assets/Scripts/HoleManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleManager : MonoBehaviour
+{
+    public static int TotalValue = 0;
+}
+
